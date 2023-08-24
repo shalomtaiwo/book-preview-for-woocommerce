83da207c52d18abf410676375d476676818e4e5b
Restyled.io <commits@restyled.io>
2023-08-24 17:46:54 +0000

    Restyled by prettier

--- assets/js/wbps-script.js
+++ assets/js/wbps-script.js
@@ -1,25 +1,28 @@
-jQuery(function(o) {
-  o(document).ready(function() {
+jQuery(function (o) {
+  o(document).ready(function () {
     var elements = jQuery(".wbps-modal-overlay, .wbps-modal");
 
-    jQuery(".wbps_popup_btnsimple_pdf")
-        .click(function() { elements.addClass("active"); });
-    jQuery(".wbps_popup_btnsimple")
-        .click(function() { elements.addClass("active"); });
+    jQuery(".wbps_popup_btnsimple_pdf").click(function () {
+      elements.addClass("active");
+    });
+    jQuery(".wbps_popup_btnsimple").click(function () {
+      elements.addClass("active");
+    });
 
-    jQuery(".close-wbps-modal")
-        .click(function() { elements.removeClass("active"); });
-    jQuery(".wbps_popup_btnsimple_pdf").click(function(d) {
+    jQuery(".close-wbps-modal").click(function () {
+      elements.removeClass("active");
+    });
+    jQuery(".wbps_popup_btnsimple_pdf").click(function (d) {
       d.stopPropagation();
       jQuery("body").addClass("overflow-hidden");
       jQuery("html").addClass("overflow-hidden");
     });
-    jQuery(".wbps_popup_btnsimple").click(function(d) {
+    jQuery(".wbps_popup_btnsimple").click(function (d) {
       d.stopPropagation();
       jQuery("body").addClass("overflow-hidden");
       jQuery("html").addClass("overflow-hidden");
     });
-    jQuery(".close-wbps-modal").click(function(d) {
+    jQuery(".close-wbps-modal").click(function (d) {
       d.stopPropagation();
       jQuery("body").removeClass("overflow-hidden");
       jQuery("html").removeClass("overflow-hidden");
@@ -27,20 +30,19 @@ jQuery(function(o) {
   });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const collapseBtn = document.getElementById("wbps-collapseBtn");
   const collapseContent = document.getElementById("wbps-collapseContent");
 
-  collapseBtn.addEventListener("click", function() {
+  collapseBtn.addEventListener("click", function () {
     if (collapseContent.style.maxHeight) {
       collapseContent.style.maxHeight = null; // Collapse the content
       collapseContent.style.padding = 0;
-      collapseContent.style.border = 0
+      collapseContent.style.border = 0;
     } else {
       collapseContent.style.maxHeight = 91 + "px"; // Expand the content
       collapseContent.style.padding = 20 + "px";
-      collapseContent.style.border = 1 + 'px' +
-                                     " solid";
+      collapseContent.style.border = 1 + "px" + " solid";
     }
   });
 });
